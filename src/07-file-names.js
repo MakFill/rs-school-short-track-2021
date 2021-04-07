b96f48275e8999546e5841f0a8398744e5831aa0
MakFil <makfil1177@gmail.com>
2021-04-07 18:47:18 +0300

    fix: add file-names

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,32 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const arr1 = names.slice();
+  const arr2 = [];
+  const newArr = [];
+
+  function findTwins(arr, t) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === t) {
+        num++;
+      }
+    }
+    return num;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      arr2.push(arr1[i]);
+    } else {
+      arr2.push(`${arr1[i]}(${findTwins(newArr, arr1[i])})`);
+      newArr.push(`${arr1[i]}(${findTwins(newArr, arr1[i])})`);
+    }
+    newArr.push(arr1[i]);
+  }
+
+  return arr2;
 }
 
 module.exports = renameFiles;
