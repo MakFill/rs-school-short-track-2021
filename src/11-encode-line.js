0f24115ef8fa6f3ec8b8eb3e1a636c0fba0ecf68
MakFil <makfil1177@gmail.com>
2021-04-08 15:50:58 +0300

    feat: add encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let counter = 1;
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i + 1] && str[i] === str[i + 1]) {
+      counter++;
+    } else {
+      if (counter > 1) {
+        newStr += counter + str[i];
+      } else {
+        newStr += str[i];
+      }
+      counter = 1;
+    }
+  }
+
+  return newStr;
 }
 
 module.exports = encodeLine;
